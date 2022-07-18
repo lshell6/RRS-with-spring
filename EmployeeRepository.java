6acf3bcd727ce6b6a0029c5b5a99b17d643a9725
KyungJoo1209 <72997626+KyungJoo1209@users.noreply.github.com>
2022-07-18 17:29:58 -0400

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rewards.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rewards.backend.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
