cbf951c1b498694ed9e50d3ba5b95f2f487f4072
Kaboateng <100966192+Kaboateng@users.noreply.github.com>
2022-08-08 19:58:18 -0400

    Update Manager.java

--- backend/model/Manager.java
+++ backend/model/Manager.java
@@ -1,15 +1,12 @@
 package com.rewards.backend.model;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.Id;
 
 @Entity
@@ -28,24 +25,29 @@ public class Manager {
 	@Column(nullable = false)
 	private String password;
 	
-//	@Column
-//	private int points_given;
+	@Column(nullable = false)
+	private String securityQuestion;
+	
+	@Column(nullable = false)
+	private String securityAnswer;
+	
+	private LocalDate passwordLastReset;
 
 	public Manager() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Manager(Long id, String name, String username, String password) {
+	public Manager(Long id, String name, String username, String password, String securityQuestion,
+			String securityAnswer, LocalDate passwordLastReset) {
 		super();
 		Id = id;
 		this.name = name;
 		this.username = username;
 		this.password = password;
-	}
-
-	public Manager(String username2, String password2, List<GrantedAuthority> list) {
-		// TODO Auto-generated constructor stub
+		this.securityQuestion = securityQuestion;
+		this.securityAnswer = securityAnswer;
+		this.passwordLastReset = passwordLastReset;
 	}
 
 	public Long getId() {
@@ -79,4 +81,36 @@ public class Manager {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-}
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+
+	public LocalDate getPasswordLastReset() {
+		return passwordLastReset;
+	}
+
+	public void setPasswordLastReset(LocalDate passwordLastReset) {
+		this.passwordLastReset = passwordLastReset;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [Id=" + Id + ", name=" + name + ", username=" + username + ", password=" + password
+				+ ", securityQuestion=" + securityQuestion + ", securityAnswer=" + securityAnswer
+				+ ", passwordLastReset=" + passwordLastReset + "]";
+	}
+	
+}	
