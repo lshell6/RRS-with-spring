27a87915303b65b6b85a12d214e5caa2d60068de
Kaboateng <100966192+Kaboateng@users.noreply.github.com>
2022-08-08 20:08:52 -0400

    Update ManagerRepository.java

--- backend/repository/ManagerRepository.java
+++ backend/repository/ManagerRepository.java
@@ -1,11 +1,21 @@
 package com.rewards.backend.repository;
 
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rewards.backend.model.Manager;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long>{
-
-	Manager getByUsername(String username);
-
+	@Query("select m from manager m where m.username=?1")
+	Manager getByManagerUsername(String username);
+	
+	@Transactional
+	@Modifying
+	@Query("update manager m SET m.password=?2,m.passwordLastReset=?3 where m.username=?1")
+	void resetPassword(String username, String password,LocalDate date);
 }
